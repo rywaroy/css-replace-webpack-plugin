76ca3f6f555f6bc740571e543c5ed311b5db3f1d
rywaroy <464485682@qq.com>
2020-07-08 18:11:32 +0800

    feat: v0.4.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,34 @@
 import { Plugin, Compiler } from 'webpack';
 interface IOption {
-    char: string;
+    prefix: string;
     ignore?: string | string[];
 }
+interface IMediaCss {
+    name: string;
+    content: string;
+    css: string[];
+}
 export default class CssReplace implements Plugin {
-    char: string;
+    prefix: string;
     ignore: string[];
+    mediaCss: IMediaCss[];
     constructor(option: IOption);
     apply(compiler: Compiler): void;
+    /**
+     * 过滤出媒体查询样式，单独处理
+     */
+    filterMediaCss(source: string): string;
+    /**
+     * 复制@media 样式
+     */
+    copyMediaCss(ignore: string): void;
+    /**
+     * 获取@media 样式
+     */
+    getMediaCss(): string;
+    /**
+     * 获取忽略的样式
+     */
+    getIgnoreCss(source: string, ignore: string): string[];
 }
 export {};
